8192545fdec8e4343bd9445db2d2d7b9b7cd8eff
Keane Dalisay <keane.dalisay0118@gmail.com>
2022-11-16 14:39:57 +0800

    Config sass and css loaders for webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,20 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: {
     index: [path.resolve(__dirname, "src/index.js")],
   },
   output: {
     path: [path.resolve(__dirname, "dist")],
     filename: "[name].bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
   },
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,22 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
+  mode: "production",
   entry: {
     index: [path.resolve(__dirname, "src/index.js")],
   },
   output: {
     path: [path.resolve(__dirname, "dist")],
     filename: "[name].bundle.js",
+  },
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
   },
 };
