c924497d4087f12498840050f1fd7934e2285a1b
Keane Dalisay <keane.dalisay0118@gmail.com>
2022-12-04 09:47:05 +0800

    Fix CleanWebpackPlugin is not a function bug

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
 const HTMLWebpackPLugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const path = require("path");
 
